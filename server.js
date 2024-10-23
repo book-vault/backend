f0c81d9e5ec9634675a6313a8c566edb8ef3ae4f
manishdotkr <officialmanishkr98@gmail.com>
2024-10-23 12:46:46 +0530

    add: added port of mysql db

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const db = mysql.createConnection({
     host: process.env.DATABASE_ENDPOINT || 'localhost',
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'password',
-    database: process.env.MYSQL_DATABASE || 'bvDB'
+    database: process.env.MYSQL_DATABASE || 'bvDB',
+    port: process.env.MYSQL_PORT
 });
 
 
